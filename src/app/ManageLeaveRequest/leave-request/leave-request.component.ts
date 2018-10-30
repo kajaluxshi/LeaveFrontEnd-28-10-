04f235c4181d5e4d9fe8baf78c03c7d310d39063
yaso selva <yasoselva1992@gmail.com>
2018-10-30 08:38:33 +0530

    get manage leave

--- src/app/ManageLeaveRequest/leave-request/leave-request.component.ts
+++ src/app/ManageLeaveRequest/leave-request/leave-request.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApplyrequestService } from 'src/app/Services/applyrequest.service';
+import { Applyrequest } from 'src/app/Models/applyrequest';
+import { InteractionService } from 'src/app/UIService/interaction.service';
 
 @Component({
   selector: 'app-leave-request',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leave-request.component.css']
 })
 export class LeaveRequestComponent implements OnInit {
-
-  constructor() { }
+  applyLeaveRequest: Applyrequest[];
+  userName:String;
+  constructor(
+    private applyrequestService: ApplyrequestService,
+    private intretionService:InteractionService
+  ) { }
 
   ngOnInit() {
+    this.intretionService.loginCredential$.subscribe(data => {
+      this.userName = data.userName;
+    });
+
+    this.getAllLeaveRequest();
   }
 
+  getAllLeaveRequest() {
+    this.applyrequestService.getAllLeaveRequest().subscribe(leavetypeDetails => {
+      console.log(leavetypeDetails);
+      this.applyLeaveRequest = leavetypeDetails;
+    });
+  }
 }
